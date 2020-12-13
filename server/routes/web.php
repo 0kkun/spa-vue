1a5bf4ce8cf566cec858ac4d2be8a4f5b30970f3
0kkun <shinji.okumoto.01@gmail.com>
2020-12-14 08:34:00 +0900

    add route for index

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,16 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// APIのURL以外のリクエストに対してはindexテンプレートを返す
+// 画面遷移はフロントエンドのVueRouterが制御する
+// PHP 7.4 から導入されたアロー関数の文法を用いている
+Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
