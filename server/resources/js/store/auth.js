d1be0a2bb8c5d132fab52c710b6bdab6e8d71f78
0kkun <shinji.okumoto.01@gmail.com>
2020-12-15 23:29:04 +0900

    edit auth.js for store

--- server/resources/js/store/auth.js
+++ server/resources/js/store/auth.js
@@ -1,11 +1,28 @@
 
-const state = {}
+const state = {
+    // ログイン済みユーザーを保持する
+    user: null
+}
 
 const getters = {}
 
-const mutations = {}
+const mutations = {
+    // user ステートの値を更新する
+    setUser (state, user) {
+        state.user = user
+    }
+}
 
-const actions = {}
+/**
+ * 会員登録 API を呼び出し、返却データを渡して setUser ミューテーションを実行することでuser ステートを更新する
+ * アクション→コミットでミューテーション呼び出し→ステート更新
+ */
+const actions = {
+    async register (context, data) {
+        const response = await axios.post('/api/register', data)
+        context.commit('setUser', response.data)
+    }
+}
 
 // ステート・ゲッター・ミューテーション・アクションを定義してストアオブジェクトとしてエクスポートする
 export default {
