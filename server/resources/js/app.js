e43a76f596b14516e6e4b34d6208ae3133e7ba74
0kkun <shinji.okumoto.01@gmail.com>
2020-12-15 11:54:32 +0900

    edit app.js for use vuex preparing

--- server/resources/js/app.js
+++ server/resources/js/app.js
@@ -3,12 +3,15 @@ require('./bootstrap');
 import Vue from 'vue'
 // ルーティングの定義をインポートする
 import router from './router'
+// VuexのStoreをインポートする
+import store from './store'
 // ルートコンポーネントをインポートする
 import App from './App.vue'
 
 new Vue({
     el: '#app',
     router, // ルーティングの定義を読み込む
+    store,  // VuexのStoreを使用する準備
     components: { App }, // ルートコンポーネントの使用を宣言する
     template: '<App />' // ルートコンポーネントを描画する
 })
