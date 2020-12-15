7a5605a9aaf7d04183ab37cfceef78dad52207f7
0kkun <shinji.okumoto.01@gmail.com>
2020-12-16 00:26:00 +0900

    edit app.js for current user check before generate Vue instance

--- server/resources/js/app.js
+++ server/resources/js/app.js
@@ -9,10 +9,18 @@ import store from './store'
 // ルートコンポーネントをインポートする
 import App from './App.vue'
 
-new Vue({
-    el: '#app',
-    router, // ルーティングの定義を読み込む
-    store,  // VuexのStoreを使用する準備
-    components: { App }, // ルートコンポーネントの使用を宣言する
-    template: '<App />' // ルートコンポーネントを描画する
-})
+
+const createApp = async () => {
+    await store.dispatch('auth/currentUser')
+
+    new Vue({
+        el: '#app',
+        router, // ルーティングの定義を読み込む
+        store,  // VuexのStoreを使用する準備
+        components: { App }, // ルートコンポーネントの使用を宣言する
+        template: '<App />'  // ルートコンポーネントを描画する
+    })
+}
+
+// アプリ起動時、Vue インスタンス生成前に currentUser アクション呼び出し
+createApp()
