3b20cc951eb31e14dd051503c04dea06e5bba3fb
0kkun <shinji.okumoto.01@gmail.com>
2020-12-15 11:47:26 +0900

    create auth.js for vuex

---
+++ server/resources/js/store/auth.js
@@ -0,0 +1,17 @@
+
+const state = {}
+
+const getters = {}
+
+const mutations = {}
+
+const actions = {}
+
+// ステート・ゲッター・ミューテーション・アクションを定義してストアオブジェクトとしてエクスポートする
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions
+}
