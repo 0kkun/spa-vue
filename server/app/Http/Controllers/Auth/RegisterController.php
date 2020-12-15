98d4d1c2276504acba71226946eaccbc149073e3
0kkun <shinji.okumoto.01@gmail.com>
2020-12-15 10:16:20 +0900

    add registered method into RegisterController

--- server/app/Http/Controllers/Auth/RegisterController.php
+++ server/app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
@@ -69,5 +70,19 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+
+    /**
+     * RegistersUsersトレイトにて、ユーザー登録した後にこのregisteredメソッドが呼ばれるようになっている。
+     * そのメソッドの中身をここで上書きして実装する
+     *
+     * @param Request $request
+     * @param [type] $user
+     * @return void
+     */
+    protected function registered(Request $request, $user)
+    {
+        return $user;
     }
 }
