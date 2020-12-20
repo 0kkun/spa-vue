097ce509136dfc4c7614464ba0c784026a92c94b
0kkun <shinji.okumoto.01@gmail.com>
2020-12-20 17:47:44 +0900

    edit Navbar for show form component

--- server/resources/js/components/Navbar.vue
+++ server/resources/js/components/Navbar.vue
@@ -5,7 +5,7 @@
         </RouterLink>
         <div class="navbar__menu">
             <div v-if="isLogin" class="navbar__item">
-                <button class="button">
+                <button class="button" @click="showForm = ! showForm">
                     <i class="icon ion-md-add"></i>
                     Submit a photo
                 </button>
@@ -23,12 +23,23 @@
                 </RouterLink>
             </div>
         </div>
+        <PhotoForm v-model="showForm" />
     </nav>
 </template>
 
 
 <script>
+import PhotoForm from './PhotoForm.vue'
+
 export default {
+    components: {
+        PhotoForm
+    },
+    data () {
+        return {
+            showForm: false
+        }
+    },
     // computed からstoreで定義したゲッターを参照している
     computed: {
         isLogin () {
