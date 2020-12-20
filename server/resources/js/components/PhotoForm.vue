610bd951ad480c54400e138f95c0db7963001a2f
0kkun <shinji.okumoto.01@gmail.com>
2020-12-20 17:17:46 +0900

    add props for recieve value into PhotoForm

--- server/resources/js/components/PhotoForm.vue
+++ server/resources/js/components/PhotoForm.vue
@@ -1,13 +1,25 @@
 
 <template>
-    <div class="photo-form">
+    <div v-show="value" class="photo-form">
         <h2 class="title">Submit a photo</h2>
         <form class="form">
-        <input class="form__item" type="file">
-        <div class="form__button">
-            <button type="submit" class="button button--inverse">submit</button>
-        </div>
+            <input class="form__item" type="file">
+            <div class="form__button">
+                <button type="submit" class="button button--inverse">submit</button>
+            </div>
         </form>
     </div>
 </template>
 
+<script>
+export default {
+    // propsは親コンポーネントから子コンポーネントにデータの受け渡しする時に使う
+    // このコンポーネントの表示 / 非表示を（value を渡す）親コンポーネント側で制御できるよう
+    props: {
+        value: {
+            type: Boolean,
+            required: true
+        }
+    }
+}
+</script>
